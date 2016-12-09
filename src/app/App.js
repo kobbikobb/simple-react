61c74bfbd009dc5b780a89100dd102a5b875ae71
jakobjo <jakobjo@temposoftware.com>
2016-12-09 22:56:18 +0000

    Initialize repository

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Like from './Like';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <p>Hello React!</p>
+        <Like />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const APP_DIR = path.resolve(__dirname, 'src/app');
+
+const config = {
+	entry: [
+  	'webpack-hot-middleware/client',
+ 		path.join(APP_DIR, "/index.js") // Your appʼs entry point
+	],
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'bundle.js',
+		publicPath: '/dist/'
+	},
+	module: {
+		loaders: [
+			{
+				test : /\.css$/,
+				loader : 'style!css'
+			},
+			{
+				test : /\.js?$/,
+				include : APP_DIR,
+				loaders : ['react-hot', 'babel']
+			}
+		]
+	},
+	plugins: [
+  	new webpack.HotModuleReplacementPlugin()
+	]
+};
+
+module.exports = config;
